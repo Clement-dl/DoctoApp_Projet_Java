91a10c92818d69593b7033e99e6f66b012ce35fa
Clément Dalberto <clement.dalberto@gmail.com>
2025-04-22 20:10:27 +0200

    Page D'Accueil V2 Page Generaliste V1

--- src/Controller/Generaliste.java
+++ src/Controller/Generaliste.java
@@ -1,29 +1,31 @@
 package Controller;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.Node;
 import javafx.stage.Stage;
 
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import javafx.event.ActionEvent;
 
-public class Accueil {
+
+public class Generaliste {
 
     @FXML
-    public void goToGeneraliste(ActionEvent event) {
+    public void goToAccueil(ActionEvent event) {
         try {
-            File fxml = new File("src/View/Generaliste.fxml");
+            File fxml = new File("src/View/Accueil.fxml");
             URL fxmlUrl = fxml.toURI().toURL();
             FXMLLoader loader = new FXMLLoader(fxmlUrl);
             Parent root = loader.load();
 
             Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-            stage.setTitle("Généraliste");
+            stage.setTitle("Accueil");
             stage.setScene(new Scene(root));
             stage.setFullScreen(true);
             stage.show();
