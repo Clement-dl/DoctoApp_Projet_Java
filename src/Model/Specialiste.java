b0957c598fc295fd11cc9482fc2c96bfb8b8cf17
EmpireLuc <bernardbanag04@gmail.com>
2025-04-27 11:09:33 +0200

    Update Specialiste.java

--- src/Model/Specialiste.java
+++ src/Model/Specialiste.java
@@ -1,24 +1,63 @@
-package Model;
+package model;
 
+/**
+ * Classe représentant un spécialiste de l'application DoctoApp.
+ */
 public class Specialiste {
-    private int id;
-    private String specialisation;
-    private String qualification;
+    private int idSpecialiste;
     private String nom;
     private String prenom;
+    private String specialite;
+    private String lieu;
 
-    public Specialiste(int id, String specialisation, String qualification, String nom, String prenom) {
-        this.id = id;
-        this.specialisation = specialisation;
-        this.qualification = qualification;
+    public Specialiste() {
+    }
+
+    public Specialiste(int idSpecialiste, String nom, String prenom, String specialite, String lieu) {
+        this.idSpecialiste = idSpecialiste;
         this.nom = nom;
+        this.prenom = prenom;
+        this.specialite = specialite;
+        this.lieu = lieu;
+    }
+
+    public int getIdSpecialiste() {
+        return idSpecialiste;
+    }
+
+    public void setIdSpecialiste(int idSpecialiste) {
+        this.idSpecialiste = idSpecialiste;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
         this.prenom = prenom;
     }
 
-    public int getId() { return id; }
-    public String getSpecialisation() { return specialisation; }
-    public String getQualification() { return qualification; }
-    public String getNomComplet() { return prenom + " " + nom; }
-    public String getPrenom() { return prenom; }
-    public String getNom() { return nom; }
+    public String getSpecialite() {
+        return specialite;
+    }
+
+    public void setSpecialite(String specialite) {
+        this.specialite = specialite;
+    }
+
+    public String getLieu() {
+        return lieu;
+    }
+
+    public void setLieu(String lieu) {
+        this.lieu = lieu;
+    }
 }
